bba50a914886581935e4fd6ec0005b7146dec88e
Janusath <93092084+Janusath@users.noreply.github.com>
2025-06-14 13:55:45 +0530

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,34 +1,35 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <title>@yield('title','Home Page')</title>
+    <title>@yield('title', 'Home Page')</title>
     <link rel="shortcut icon" href="assets/img/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/plugins/feather/feather.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/plugins/icons/flags/flags.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap-datetimepicker.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/plugins/select2/css/select2.min.css')}}">
-      {{-- datatable css link --}}
-    <link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
-    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" /> --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icons/flags/flags.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
 
-    {{-- toastr alert css link--}}
+    {{-- datatable css external link --}}
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" /> --}}
+    {{-- or internal template link --}}
+    <link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
+
+    {{-- toastr alert css link --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
 </head>
 
 <body>
 
-    {{--this link will come at the first  --}}
-    <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
+    {{-- this link will come at the first  --}}
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
 
     <div class="main-wrapper">
 
@@ -36,8 +37,8 @@
         @include('includes.header')
 
 
-         <!-- sidebar  -->
-          @include('includes.sidebar')
+        <!-- sidebar  -->
+        @include('includes.sidebar')
 
 
         <div class="page-wrapper">
@@ -47,24 +48,20 @@
 
 
             <!-- footer -->
-             @include('includes.footer')
+            @include('includes.footer')
 
         </div>
 
     </div>
 
-
-
-
-    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/js/feather.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/select2/js/select2.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/moment/moment.min.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap-datetimepicker.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/apexchart/apexcharts.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/apexchart/chart-data.js')}}"></script>
-
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/apexchart/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/apexchart/chart-data.js') }}"></script>
 
     {{-- datatable js external link --}}
     {{-- <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script> --}}
@@ -72,17 +69,13 @@
     <script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="assets/plugins/datatables/datatables.min.js"></script>
 
-    {{-- toastr alert css link--}}
+    {{-- toastr alert css link --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     {{-- toastr alert js link --}}
 
-     <script src="{{asset('assets/js/script.js')}}"></script>
-
-
+    <script src="{{ asset('assets/js/script.js') }}"></script>
 
 </body>
 
 </html>
-
-
