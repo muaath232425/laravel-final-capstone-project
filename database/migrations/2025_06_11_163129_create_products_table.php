7d8cd73ff8fee5fddbcbc9074d1b1bb5f1fca471
ROOT MASTER <mmuaath30@gmail.com>
2025-06-16 12:05:07 +0530

    dddddddd
    dddddddddddd

--- database/migrations/2025_06_11_163129_create_products_table.php
+++ database/migrations/2025_06_11_163129_create_products_table.php
@@ -10,12 +10,14 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
